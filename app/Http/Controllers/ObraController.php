fc82ee77e2e81bc58b9189c177aed44eaa7b9185
Alvaro <alvgonzadel@gmail.com>
2022-03-30 18:05:16 +0200

    FirstJson done

--- app/Http/Controllers/ObraController.php
+++ app/Http/Controllers/ObraController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
+use App\Models\Email;
+use App\Models\Obra;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ObraController extends Controller
 {
-    //
+    public function index($id){
+
+        $data = Obra::with(['cliente', 'email'])->findOrFail(1);
+
+        return new JsonResponse($data);
+    }
 }
--- app/Models/Obra.php
+++ app/Models/Obra.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Obra extends Model
 {
     use HasFactory;
+
+    protected $hidden = array('cliente_id', 'created_at', 'updated_at');
+
+    protected $fillable = [
+
+    ];
+
+    public function Cliente(){
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function Email(){
+        return $this->belongsToMany(Email::class, '');
+    }
 }
